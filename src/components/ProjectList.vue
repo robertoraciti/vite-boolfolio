ac675f35c90421832f06f57d59addce5c031eed2
robertoraciti <rob.raciti97@gmail.com>
2023-11-05 21:58:23 +0100

    Bonus

--- src/components/ProjectList.vue
+++ src/components/ProjectList.vue
@@ -10,6 +10,12 @@ export default {
       api: {
         baseUrl: "http://127.0.0.1:8000/api/",
       },
+
+      pagination: {
+        links: null,
+        prev: null,
+        next: null,
+      },
     };
   },
 
@@ -21,6 +27,9 @@ export default {
     fetchProjects(uri = this.api.baseUrl + "projects") {
       axios.get(uri).then((response) => {
         this.projects = response.data.data;
+        this.pagination.links = response.data.links;
+        this.pagination.prev = response.data.prev_page_url;
+        this.pagination.next = response.data.next_page_url;
       });
     },
   },
@@ -32,9 +41,22 @@ export default {
 </script>
 
 <template>
-  <h2>Lista progetti</h2>
-  <div class="row row-cols-3 g-4">
-    <ProjectCard v-for="project in projects" :project="project" />
+  <div class="container">
+    <h2 class="mb-5">Lista progetti</h2>
+    <div class="row row-cols-3 g-4">
+      <ProjectCard v-for="project in projects" :project="project" />
+    </div>
+    <nav class="mt-5" aria-label="Page navigation example">
+      <ul class="pagination">
+        <li
+          v-for="link in pagination.links"
+          @click="fetchProjects(link.url)"
+          class="page-item"
+        >
+          <a class="page-link" href="#" v-html="link.label"></a>
+        </li>
+      </ul>
+    </nav>
   </div>
 </template>
 
