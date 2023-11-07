561af176f4b4efef60262abc2ea1d51498519e2c
robertoraciti <rob.raciti97@gmail.com>
2023-11-07 16:31:33 +0100

    Milestone 1-2

--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 import { createApp } from "vue";
 import "./assets/scss/styles.scss";
+
+import { router } from "./router";
+
 import App from "./App.vue";
 import * as bootstrap from "bootstrap";
 
-createApp(App).mount("#app");
+createApp(App).mount("#app").use(router);
---
+++ src/router/index.js
@@ -0,0 +1,22 @@
+import { createWebHistory, createRouter } from "vue-router";
+import HomePage from "../pages/HomePage.vue";
+import PortfolioPage from "../pages/PortfolioPage.vue";
+
+const router = createRouter({
+  history: createWebHistory(),
+
+  routes: [
+    {
+      name: "homepage",
+      path: "/",
+      component: HomePage,
+    },
+    {
+      name: "portfolio",
+      path: "/portfolio",
+      component: PortfolioPage,
+    },
+  ],
+});
+
+export { router };
